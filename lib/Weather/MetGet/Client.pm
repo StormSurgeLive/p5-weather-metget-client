d49418b5bca1404e6618c8ee54f909ba3276bcd2
wwlwpd <46434714+wwlwpd@users.noreply.github.com>
2021-10-27 01:04:31 -0500

    cleaned up, moved auth info to ~/.metgetrc and updated POD

--- lib/Weather/MetGet/Client.pm
+++ lib/Weather/MetGet/Client.pm
@@ -10,6 +10,8 @@ sub new {
     my $pkg  = shift;
     my %self = @_;
     my $self = bless \%self, $pkg;
+
+    # 'x-api-key' is how authentication happens at API request time
     $self->{__ua} = HTTP::Tiny->new( default_headers => { q{x-api-key} => $self->{apikey} } );
     return $self;
 }
@@ -17,25 +19,47 @@ sub new {
 sub status {
     my $self     = shift;
     my $url      = sprintf( "%s%s", $self->{baseurl}, q{/status} );
-    my $response = $self->{__ua}->get($url);
-    return $response->{content};
+    my $response = h2o $self->{__ua}->get($url);
+    if ( not $response->success ) {
+        die sprintf( qq{%s %s}, $response->status, $response->reason );
+    }
+    return $response->content;
 }
 
 1;
 __END__
 =head1 NAME
 
-Weather::MetGet::Client - Perl extension for blah blah blah
+Weather::MetGet::Client - Perl client for C<MetGet> service. 
 
 =head1 SYNOPSIS
 
   use Weather::MetGet::Client;
-  blah blah blah
+  my @OPTIONS = ( apikey => $mykey, baseurl => $baseurl );
+  my $metget  = Weather::MetGet::Client->new(@OPTIONS);
+  
+  # call /status end point
+  my $status = $metget->status;
 
 =head1 DESCRIPTION
 
-=head1 SEE ALSO
+API client implementing some set of C<MetGet> service calls,
+used primarily to support the commandline client included in
+this distribution, C<mg>.
 
-=head1 COPYRIGHT AND LICENSE
+=head1 LICENSE AND COPYRIGHT
 
-=cut
+This file is part of the ADCIRC Surge Guidance System (ASGS).
+
+The ASGS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+ASGS is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the ASGS.  If not, see <http://www.gnu.org/licenses/>.
